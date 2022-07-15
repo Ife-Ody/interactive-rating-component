81324b64cb997d8746015bf1f27a262269564532
Ifeoluwa Odubela <i.odubela@gmail.com>
2022-07-16 00:50:27 +0100

    pre-sumbit phase done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+// pages/_app.js
+import { ChakraProvider } from "@chakra-ui/react";
 
+// 1. Import the extendTheme function
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "hsl(228, 45%, 44%)"
+  },
+    darkBlue: "#262E38",
+lightGrey: "#969FAD",
+mediumGray: "#7C8798",
+orange: "#FC7614",
+pureWhite: "#FFFFFF",
+veryDarkBlue: "#131518",
+  
+};
+
+const theme = extendTheme({ colors });
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
