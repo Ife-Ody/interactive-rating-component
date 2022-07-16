ae8bcc762949a319b0247ce9e972ef8cf7033eb5
Ifeoluwa Odubela <i.odubela@gmail.com>
2022-07-16 01:18:03 +0100

    fixed the issue with the theme file

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,6 @@
 
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-import theme from "./theme";
 
 export default class Document extends NextDocument {
   render() {
@@ -10,8 +9,6 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head />
         <body>
-          {/* 👇 Here's the script */}
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
